776480aa62a8e402558f872e73e882a65b431ad0
freddydc <freddydiaz.liv@gmail.com>
2022-05-19 11:49:32 -0500

    add: Request methods

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import express from 'express'
 const main = express()
 const port = 5000
 
-main.use('/', (req, res) => {
-  res.send('Welcome')
+const router = express.Router()
+
+main.use(router)
+
+router.get('/message', (req, res) => {
+  res.send('Message list')
+})
+
+router.post('/message', (req, res) => {
+  res.send('Added message')
 })
 
 main.listen(port, () => {
