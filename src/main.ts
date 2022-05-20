6c8896c31a1d4ebe802cbbf5dda172b212c9f8c6
freddydc <freddydiaz.liv@gmail.com>
2022-05-20 11:48:51 -0500

    Manage error response

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,13 @@ router.post('/message', (req, res) => {
   console.log(req.query)
   console.log(req.body)
   if (req.query.error == 'test') {
-    response.error(req, res, 'Test error success', 400)
+    response.error(
+      req,
+      res,
+      'An unexpected error ocurred',
+      500,
+      'Error details'
+    )
   } else {
     response.success(req, res, 'Added message successfully', 201)
   }
--- src/network/response.ts
+++ src/network/response.ts
@@ -11,7 +11,8 @@ type Error = <T>(
   req: Request,
   res: Response,
   message: T,
-  status?: number
+  status: number,
+  details: string
 ) => void
 
 const success: Success = (req, res, message, status) => {
@@ -21,7 +22,8 @@ const success: Success = (req, res, message, status) => {
   })
 }
 
-const error: Error = (req, res, message, status) => {
+const error: Error = (req, res, message, status, details) => {
+  console.log(`> [ ERROR RESPONSE ] = ${details}`)
   res.status(status || 500).send({
     error: message,
     body: ''
